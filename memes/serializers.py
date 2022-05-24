d2fb9c5fba7a07d29fb36bb8403f1de2ac83e13b
0djentd <58089540+0djentd@users.noreply.github.com>
2022-05-25 02:00:33 +0300

    random meme now uses drf.

---
+++ memes/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Meme
+
+
+class MemeSerializer(serializers.ModelSerializer):
+    extra_serializer_field = serializers.SerializerMethodField(read_only=True)
+    class Meta():
+        model = Meme
+        fields = ["name", "joke", "description", "extra_serializer_field"]
+
+    def get_extra_serializer_field(self, obj):
+        return len(obj.name)
--- memes/views.py
+++ memes/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpRequest
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Meme
+from .serializers import MemeSerializer
 import json
 
 
-def random_meme(req: HttpRequest) -> JsonResponse:
+# simple api
+# def random_meme(req: HttpRequest) -> JsonResponse:
+#     meme = Meme.objects.first()
+#     # simplest version
+#     # data = {'name': meme.name}
+#     # second simplest version
+#     data = model_to_dict(meme, fields=["name"])
+#     return JsonResponse(data)
+
+# drf api
+@api_view(["GET"])
+def random_meme(req: HttpRequest) -> Response:
     meme = Meme.objects.first()
-    data = {'name': meme.name}
-    return JsonResponse(data)
+    data = MemeSerializer(meme).data
+    return Response(data)
