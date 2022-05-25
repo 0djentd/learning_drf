c3bdb9ad2ff76f0b922ba1d30f9f30d4f3ad70fd
0djentd <58089540+0djentd@users.noreply.github.com>
2022-05-25 05:50:00 +0300

    modified:   memes/serializers.py modified:   memes/views.py

--- memes/serializers.py
+++ memes/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Meme
 
 
+# same thing but can be referenced by url
+# class MemeSerializer(serializers.HyperlinkedModelSerializer):
 class MemeSerializer(serializers.ModelSerializer):
     extra_serializer_field = serializers.SerializerMethodField(read_only=True)
     class Meta():
--- memes/views.py
+++ memes/views.py
@@ -4,26 +4,34 @@ from django.http import JsonResponse
 from django.http import HttpRequest
 from django.forms.models import model_to_dict
 
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import generics
+from rest_framework import generics, mixins, permissions, response, decorators, authentication
 
 from .models import Meme
 from .serializers import MemeSerializer
 
+# TODO:
+# check out drf api doc for:
+# mixins attrs
+# permissions classes (also check out django obj permissions asap)
+# authentication classes
+# how to deploy django (wsgi asgi nginx apache docker)
 
-# simple api
+# ===========
+# basic api.
+# ===========
 # def random_meme(req: HttpRequest) -> JsonResponse:
 #     meme = Meme.objects.first()
-#     # simplest version
+#     # simplest version.
 #     # data = {'name': meme.name}
-#     # second simplest version
+#     # second simplest version.
 #     data = model_to_dict(meme, fields=["name"])
 #     return JsonResponse(data)
 
-# drf api basic serializer
-@api_view(["GET", "POST"])
-def random_meme(req: HttpRequest) -> Response:
+# ===========
+# api_view decorator + basic serializers usage.
+# ===========
+@decorators.api_view(["GET", "POST"])
+def random_meme(req: HttpRequest) -> response.Response:
     data = {}
     if req.method == "GET":
         meme = Meme.objects.all().order_by("?").first()
@@ -34,10 +42,14 @@ def random_meme(req: HttpRequest) -> Response:
         if meme.is_valid(raise_exception=True):
             meme.save()
             data = meme.data
-    return Response(data)
+    return response.Response(data)
 
 
-# Class-based view.
+# ===========
+# Class-based views.
+# ===========
+# Concrete views.
+# ============
 # get
 # class random_meme_cbv(generics.RetrieveAPIView):
 # get, put
@@ -48,6 +60,8 @@ def random_meme(req: HttpRequest) -> Response:
 class _MemeDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Meme.objects.all()
     serializer_class = MemeSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 meme_detail_view = _MemeDetailAPIView.as_view()
 
@@ -62,10 +76,30 @@ class _MemeListCreateAPIView(generics.ListCreateAPIView):
 meme_list_create_view = _MemeListCreateAPIView.as_view()
 
 
-# no need if using ListCreateAPIView
+# no need if using ListCreateAPIView.
 # post
 # class _MemeCreateAPIView(generics.CreateAPIView):
 #     queryset = Meme.objects.all()
 #     serializer_class = MemeSerializer
 #
 # meme_create_view = _MemeCreateAPIView.as_view()
+
+
+# ===========
+# Mixins
+# ===========
+# basically the same thing as previous type of views.
+# kinda better idk, might as well use it.
+# actually prev. thing is just a class like this one.
+# class _MemeListCreateAPIView(
+#         mixins.ListModelMixin,
+#         mixins.CreateModelMixin,
+#         generics.GenericAPIView):
+#     pass
+
+# ===========
+# Basic APIView
+# ===========
+# kinda same thing, but without default implementation. idk why tho.
+# class _MemeListCreateAPIView(generics.APIView):
+#   get(req) -> res:
